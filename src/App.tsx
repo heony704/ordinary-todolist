b459c9b539c244847e11563f6b409961b65f3a45
heony704 <heony704@gmail.com>
2023-02-14 14:56:25 +0000

    Feat: add foundation components
    - add react-router-dom
    - add necessary pages and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import TodolistPage from './pages/TodolistPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
-function App() {
-  return <div className="App">hello hi</div>;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TodolistPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
---
+++ src/pages/TodolistPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Todo from '../components/Todo';
+
+export default function TodolistPage() {
+  return (
+    <>
+      <Todo />
+      <Todo />
+      <Todo />
+    </>
+  );
+}
