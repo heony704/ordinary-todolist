547f3197675fb866a29f85cd72a25ae26de1535a
heony704 <heony704@gmail.com>
2023-02-16 07:28:25 +0000

    Style: pages and todo component with tailwind
    - add tailwindcss
    - style whole pages and todo component

--- src/pages/TodolistPage.tsx
+++ src/pages/TodolistPage.tsx
@@ -3,10 +3,24 @@ import Todo from '../components/Todo';
 
 export default function TodolistPage() {
   return (
-    <>
-      <Todo />
-      <Todo />
-      <Todo />
-    </>
+    <div className="flex flex-col items-center">
+      <div className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        Todolist
+      </div>
+      <form className="space-y-4 md:space-y-6">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="name@company.com"
+          />
+      </form>
+      <div>
+        <Todo />
+        <Todo />
+        <Todo />
+      </div>
+    </div>
   );
 }
