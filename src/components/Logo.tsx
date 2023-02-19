c1a8dfe78201f688cb513cac6059b3594636aa2a
heony704 <heony704@gmail.com>
2023-02-19 15:45:57 +0900

    Feat: Logo 클릭하면 홈화면으로 이동

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Logo() {
   return (
-    <div className="flex items-center text-2xl font-semibold text-gray-900 dark:text-white">
+    <Link
+      to="/"
+      className="flex items-center text-2xl font-semibold text-gray-900 select-none dark:text-white"
+    >
       Todolist
-    </div>
+    </Link>
   );
 }
