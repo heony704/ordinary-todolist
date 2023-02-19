12c1754d20f4a50b2f6a44271544eecb95673081
heony704 <heony704@gmail.com>
2023-02-19 13:19:39 +0900

    Feat: TodoList 페이지 UI 구현
    - react-icons 패키지 추가
    - TodoListPage UI 스타일 수정
    - Todo 수정
    - Todo type 추가

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Todo() {
+export default function Todo({ contents }: Todo) {
   return (
-    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-      Todo
+    <div className="flex w-full p-2.5 text-sm text-gray-900 bg-white rounded-lg border border-gray-300 dark:bg-gray-700  dark:border-gray-600 dark:text-white">
+      <pre>{contents}</pre>
     </div>
   );
 }
--- src/pages/TodolistPage.tsx
+++ src/pages/TodolistPage.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
+import { TbArrowDown } from 'react-icons/tb';
 import Todo from '../components/Todo';
 
 export default function TodolistPage() {
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+    <div className="flex flex-col items-center px-6 max-w-3xl mx-auto">
+      <div className="flex items-center py-6 text-2xl font-semibold text-gray-900 dark:text-white">
         Todolist
       </div>
-      <form className="space-y-4 md:space-y-6">
+      <div className="relative w-full">
         <input
-          type="email"
-          name="email"
-          id="email"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="name@company.com"
+          className="block p-2.5 pr-[3.3rem] w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+          placeholder="Input todos..."
         />
-      </form>
-      <div>
-        <Todo />
-        <Todo />
-        <Todo />
+        <button
+          type="submit"
+          className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-primary-600 rounded-r-lg border border-primary-600 hover:bg-primary-700 focus:ring focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+        >
+          <TbArrowDown className="w-5 h-5" />
+        </button>
+      </div>
+      <div className="w-full pt-10 space-y-6">
+        <Todo contents="todo 1" />
+        <Todo contents="todo 2" />
+        <Todo contents="todo 3" />
       </div>
     </div>
   );
