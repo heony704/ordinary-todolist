c9be325097736125c787a49876509155c2962273
heony704 <heony704@gmail.com>
2023-02-22 21:55:26 +0900

    Style: label과 input 사이 간격 추가

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -47,14 +47,14 @@ function FormInput({
   return (
     <label
       htmlFor={name}
-      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      className="block text-sm font-medium text-gray-900 dark:text-white"
     >
       {label}
       <input
         type={type}
         name={name}
         id={name}
-        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring-1 focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="mt-2 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring-1 focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder={placeholder}
         value={value}
         onChange={onChange}
