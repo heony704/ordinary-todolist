d4e84dcdba9a3b56a49fde79367c869c339f17df
heony704 <heony704@gmail.com>
2023-02-24 13:52:19 +0900

    Refactor: Token 관련 함수 분리

--- src/hooks/useAuthRouting.ts
+++ src/hooks/useAuthRouting.ts
@@ -1,14 +1,13 @@
 import { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { getToken } from 'src/utils/token';
 
 export default function useAuthRouting() {
   const navigate = useNavigate();
   const location = useLocation();
 
   useEffect(() => {
-    if (location.pathname !== '/' && localStorage.getItem('access_token'))
-      navigate('/');
-    else if (location.pathname === '/' && !localStorage.getItem('access_token'))
-      navigate('/login');
+    if (location.pathname !== '/' && getToken()) navigate('/');
+    else if (location.pathname === '/' && !getToken()) navigate('/login');
   }, [navigate]);
 }
