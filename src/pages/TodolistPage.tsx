afa48a4a908941581f71f97e7d261e8d5ead9e9a
Seungheon Lee <36994104+heony704@users.noreply.github.com>
2023-02-28 13:10:03 +0900

    Feat: todo CRUD (#2)
    * Feat
    - Todo CRUD 구현
    - Todo toggle 구현
    
    *Refactor
    - Todo 타입 수정
    - axios 인터셉터 제거, 각 파일에서 axios 인스턴스 생성
    - Logo 컴포넌트 제거
    - LoginPage, RegisterPage의 중복되는 부분을 레이아웃으로 만들어서 AuthPage 컴포넌트로 분리

--- src/pages/TodolistPage.tsx
+++ src/pages/TodolistPage.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import useAuthRouting from 'src/hooks/useAuthRouting';
-import Logo from 'src/components/Logo';
-import { HiOutlineChevronDown } from 'react-icons/hi';
-import Todo from 'src/components/Todo';
 import { removeToken } from 'src/utils/token';
+import TodoList from 'src/components/TodoList';
+import TodoInput from 'src/components/TodoInput';
 
-export default function TodolistPage() {
+export default function TodoListPage() {
   useAuthRouting();
 
   const navigate = useNavigate();
@@ -16,9 +15,14 @@ export default function TodolistPage() {
   };
 
   return (
-    <div className="flex flex-col items-center px-6 pt-6 max-w-3xl mx-auto">
+    <div className="flex flex-col items-center px-6 pt-6 pb-10 max-w-3xl mx-auto">
       <div className="flex justify-between items-center w-full">
-        <Logo />
+        <Link
+          to="/"
+          className="flex items-center text-2xl font-semibold text-black select-none"
+        >
+          Todolist
+        </Link>
         <button
           type="button"
           className="button-rounded-sm button-gray"
@@ -27,23 +31,8 @@ export default function TodolistPage() {
           Logout
         </button>
       </div>
-      <div className="relative w-full mt-6">
-        <input
-          className="block p-2.5 pr-[3.3rem] w-full text-sm input-rounded input-white"
-          placeholder="Input todos..."
-        />
-        <button
-          type="submit"
-          className="absolute top-0 right-0 p-2.5 text-sm font-medium rounded-r-lg button-primary border border-primary-600"
-        >
-          <HiOutlineChevronDown className="w-5 h-5" />
-        </button>
-      </div>
-      <div className="w-full mt-10 space-y-6">
-        <Todo contents="todo 1" />
-        <Todo contents="todo 2" />
-        <Todo contents="todo 3" />
-      </div>
+      <TodoInput />
+      <TodoList />
     </div>
   );
 }
