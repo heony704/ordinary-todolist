9292cfc20133bcbcecc0152cc449054075d4caed
heony704 <heony704@gmail.com>
2023-02-28 13:05:06 +0900

    Refactor: 로그인, 회원가입 페이지 & 로고 컴포넌트 제거
    - LoginPage, RegisterPage의 중복되는 부분을 레이아웃으로 만들어서 AuthPage 파일로 분리
    - Logo 컴포넌트가 독립적으로 존재할 필요가 없다고 판단하여 제거
    - TodolistPage => TodoListPage 이름 수정

--- src/pages/TodolistPage.tsx
+++ src/pages/TodolistPage.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import useAuthRouting from 'src/hooks/useAuthRouting';
 import { removeToken } from 'src/utils/token';
-import Logo from 'src/components/Logo';
 import TodoList from 'src/components/TodoList';
 import TodoInput from 'src/components/TodoInput';
 
-export default function TodolistPage() {
+export default function TodoListPage() {
   useAuthRouting();
 
   const navigate = useNavigate();
@@ -18,7 +17,12 @@ export default function TodolistPage() {
   return (
     <div className="flex flex-col items-center px-6 pt-6 pb-10 max-w-3xl mx-auto">
       <div className="flex justify-between items-center w-full">
-        <Logo />
+        <Link
+          to="/"
+          className="flex items-center text-2xl font-semibold text-black select-none"
+        >
+          Todolist
+        </Link>
         <button
           type="button"
           className="button-rounded-sm button-gray"
