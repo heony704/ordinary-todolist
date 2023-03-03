ba2356c39c583d3c05433b103477ef07daf704a3
heony704 <heony704@gmail.com>
2023-03-03 20:22:47 +0900

    Chore: import 절대경로로 통일

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import TodolistPage from './pages/TodolistPage';
-import AuthPage from './pages/AuthPage';
-import LoginForm from './components/LoginForm';
-import RegisterForm from './components/RegisterForm';
+import TodolistPage from 'src/pages/TodolistPage';
+import AuthPage from 'src/pages/AuthPage';
+import LoginForm from 'src/components/LoginForm';
+import RegisterForm from 'src/components/RegisterForm';
 
 export default function App() {
   return (
