9c6bdf9a5588a41b1f797b5caf820a4ec7b76b6d
heony704 <heony704@gmail.com>
2023-03-17 21:05:39 +0900

    Feat: 로고 눌러도 리다이렉팅 안하도록 수정
    - 로그인되지 않은 상태에서는 투두리스트에 접근할 수 없음
    - 따라서 로그인, 회원가입 페이지에서 로고를 클릭했을 때 투두리스트로 리다이렉팅하는 기능은 쓸모가 없다고 판단
    - 로그를 클릭해도 리다이렉팅하지 않도록 수정

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 export default function AuthPage() {
   return (
     <div className="flex flex-col items-center justify-center px-6 py-8 space-y-6 mx-auto md:h-screen lg:py-0">
-      <Link
-        to="/"
-        className="flex items-center text-2xl font-semibold text-black select-none"
-      >
+      <h1 className="flex items-center text-2xl font-semibold text-black select-none">
         Todolist
-      </Link>
+      </h1>
       <Outlet />
     </div>
   );
