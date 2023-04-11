886109848e4447b4a68fbcc698ae0354cb884783
heony704 <heony704@gmail.com>
2023-04-12 00:05:34 +0900

    Fix: api server address because of api server rebuilding

--- src/api/fetchInstance.ts
+++ src/api/fetchInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getToken } from 'src/utils/accessToken';
 
-const BASE_URL = 'https://pre-onboarding-selection-task.shop';
+const BASE_URL = 'http://43.201.130.34:8000';
 
 const fetchInstance = axios.create({
   baseURL: BASE_URL,
