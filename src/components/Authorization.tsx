d6025a2836bfdd7fc26f4cf7fbba235749b28cfb
heony704 <heony704@gmail.com>
2023-06-24 01:56:49 +0900

    Refactor: add loading between page transitions
    Before:
    로그인 유무를 확인하기 위해 API 요청 결과가 나오기 전에 화면이 보여짐
    
    After:
    API 요청 결과가 나오기 전엔 로딩 스피너를 보여주고 결과가 나오면 결과에 따라 다른 처리를 해줌

--- src/components/Authorization.tsx
+++ src/components/Authorization.tsx
@@ -1,22 +1,40 @@
-import React, { useEffect } from 'react';
-import { useNavigate, useLocation, Outlet } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Navigate, useLocation, Outlet } from 'react-router-dom';
+import Spinner from 'src/components/Spinner';
 import { getTodos } from 'src/api/handleTodo';
 
 export default function Authorization() {
+  const [loading, setLoading] = useState(true);
+  const [logined, setLogined] = useState(false);
   const location = useLocation();
-  const navigate = useNavigate();
 
   useEffect(() => {
-    async function fetchTodoList() {
-      try {
-        await getTodos();
-        if (location.pathname !== '/') navigate('/');
-      } catch {
-        if (location.pathname === '/') navigate('/login');
-      }
-    }
-    fetchTodoList();
+    getTodos()
+      .then(() => {
+        setLogined(true);
+      })
+      .catch(() => {
+        setLogined(false);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
+
+  if (loading)
+    return (
+      <div className="flex justify-center items-center w-full h-screen">
+        <Spinner />
+      </div>
+    );
+
+  if (location.pathname === '/' && !logined) {
+    return <Navigate to="/login" />;
+  }
+
+  if (location.pathname !== '/' && logined) {
+    return <Navigate to="/" />;
+  }
 
   return <Outlet />;
 }
